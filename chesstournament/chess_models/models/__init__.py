51a6e7a6a2c4f526be98891401ea64f9f8469c6c
ErnesP <ernesto.pinnon@estudiante.uam.es>
2025-04-02 09:45:44 +0200

    arreglo de lo de alex

--- chesstournament/chess_models/models/__init__.py
+++ chesstournament/chess_models/models/__init__.py
@@ -1,19 +1,8 @@
 # chess_models/models/__init__.py
-from chess_models.constants import TournamentType, TournamentSpeed, Scores
+from chess_models.constants import TournamentType, TournamentSpeed, Scores, TournamentBoardType, RankingSystem
 from chess_models.models.player import Player, LichessAPIError
 from chess_models.models.tournament import Tournament
 from chess_models.models.round import Round
 from chess_models.models.game import Game
 from chess_models.models.referee import Referee
 
-__all__ = [
-    'Player',
-    'Tournament',
-    'Round', 
-    'Game',
-    'Referee',
-    'TournamentType',
-    'TournamentSpeed',
-    'Scores'
-    'LichessAPIError'
-]
