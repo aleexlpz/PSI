581640a13931c7ddf0a0a8a72b2167c600303562
ErnesP <ernesto.pinnon@estudiante.uam.es>
2025-04-04 10:09:20 +0200

    commit

--- chesstournament/chesstournament/settings.py
+++ chesstournament/chesstournament/settings.py
@@ -44,7 +44,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chess_models',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
