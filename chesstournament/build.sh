00d26aea29a2609e81ea73c6ec7071344b8456c1
aleexlpz <alejandro.lopez02@estudiante.uam.es>
2025-04-08 17:31:51 +0200

    render 1

---
+++ chesstournament/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python -m pip install --upgrade pip
+
+pip install -r requirements.txt
+
+python manage.py makemigrations
+python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('alumnodb', 'alumnodb@estudiante.uam.es', 'alumnodb') if not User.objects.filter(username='alumnodb').exists() else print('Superusuario ya existe')" | python manage.py shell
