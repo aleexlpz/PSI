206a8838c0ce353081bb4d8d5f70a020d4c05a45
ErnesP <ernesto.pinnon@estudiante.uam.es>
2025-04-08 10:29:49 +0200

    com

--- chesstournament/chesstournament/urls.py
+++ chesstournament/chesstournament/urls.py
@@ -20,6 +20,7 @@ from djoser.views import TokenDestroyView
 from django.contrib import admin
 from django.urls import include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth_token/login/', obtain_auth_token, name='login'),
@@ -27,5 +28,4 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')), 
     path('api/v1/', include('api.urls')),
-
 ]
