ee592cb9bf706015402a0a1d34225b5d1b92f1b8
ErnesP <ernesto.pinnon@estudiante.uam.es>
2025-05-09 22:37:21 +0200

    databases bien x2

--- chesstournament/chesstournament/settings.py
+++ chesstournament/chesstournament/settings.py
@@ -165,4 +165,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:5173',
+    'https://psip3-tdnm.onrender.com',
 ]
